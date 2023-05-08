428eb415e16fa76b8e50d9285dd98300904bb315
paragparab <125932883+paragparab@users.noreply.github.com>
2023-05-07 22:15:43 -0400

    Add files via upload

---
+++ Customer churn Prediction Project(Data Analytics).r
@@ -0,0 +1,350 @@
+#install.packages("e1071")
+#install.packages("DT")
+#install.packages("rms")
+#install.packages("MASS")
+#install.packages("ROCR")
+#install.packages("gplots")
+#install.packages("pROC")
+#install.packages("rpart")
+#install.packages("randomForest")
+#install.packages("tidyverse")
+#install.packages("nnet")
+#install.packages("neuralnet")
+#install.packages("NeuralNetTools")
+
+rm(list = ls())
+setwd("C:/Users/srira/Downloads")
+library(plyr)  
+library(rpart.plot) 
+library(caret)
+library(gridExtra) 
+library(tidyverse)
+library(rsample)
+library(e1071) 
+library(GGally)
+library(data.table)
+library(DT)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(corrplot)
+library(rms)
+library(MASS)
+library(e1071)
+library(ROCR)
+library(gplots)
+library(pROC)
+library(rpart)
+library(randomForest)
+library(ggpubr)
+library(neuralnet)
+library(nnet)
+library(NeuralNetTools)
+
+#read data
+# Read the file
+churn <- read.csv("Telco-Customer-Churn.csv")
+
+
+glimpse(churn)
+
+sapply(churn, function(x) sum(is.na(x)))
+
+churn[is.na(churn$TotalCharges),]
+sum(is.na(churn$TotalCharges))/nrow(churn)
+churn_clean <- churn[complete.cases(churn), ]
+churn_clean$SeniorCitizen <- as.factor(mapvalues(churn_clean$SeniorCitizen,
+                                                 from=c("0","1"),
+                                                 to=c("No", "Yes")))
+churn_clean$MultipleLines <- as.factor(mapvalues(churn_clean$MultipleLines, 
+                                                 from=c("No phone service"),
+                                                 to=c("No")))
+
+sapply(churn_clean, function(x) "No internet service" %in% levels(x))
+
+
+churn_clean$customerID <- NULL
+#Gender plot
+p1 <- ggplot(churn_clean, aes(x = gender)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Senior citizen plot
+p2 <- ggplot(churn_clean, aes(x = SeniorCitizen)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Partner plot
+p3 <- ggplot(churn_clean, aes(x = Partner)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Dependents plot
+p4 <- ggplot(churn_clean, aes(x = Dependents)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Plot demographic data within a grid
+grid.arrange(p1, p2, p3, p4, ncol=2)
+
+#Phone service plot
+p5 <- ggplot(churn_clean, aes(x = PhoneService)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Multiple phone lines plot
+p6 <- ggplot(churn_clean, aes(x = MultipleLines)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Internet service plot
+p7 <- ggplot(churn_clean, aes(x = InternetService)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Online security service plot
+p8 <- ggplot(churn_clean, aes(x = OnlineSecurity)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Online backup service plot
+p9 <- ggplot(churn_clean, aes(x = OnlineBackup)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Device Protection service plot
+p10 <- ggplot(churn_clean, aes(x = DeviceProtection)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Tech Support service plot
+p11 <- ggplot(churn_clean, aes(x = TechSupport)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Streaming TV service plot
+p12 <- ggplot(churn_clean, aes(x = StreamingTV)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Streaming Movies service plot
+p13 <- ggplot(churn_clean, aes(x = StreamingMovies)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Plot service data within a grid
+grid.arrange(p5, p6, p7,
+             p8, p9, p10,
+             p11, p12, p13,
+             ncol=3)
+
+
+#Contract status plot
+p14 <- ggplot(churn_clean, aes(x = Contract)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Paperless billing plot
+p15 <- ggplot(churn_clean, aes(x = PaperlessBilling)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Payment method plot
+p16 <- ggplot(churn_clean, aes(x = PaymentMethod)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+#Plot contract data within a grid
+grid.arrange(p14, p15, p16, ncol=1)
+
+
+#Tenure histogram
+p17 <- ggplot(data = churn_clean, aes(tenure, color = Churn))+
+  geom_freqpoly(binwidth = 5, size = 1)
+
+#Monthly charges histogram
+p18 <- ggplot(data = churn_clean, aes(MonthlyCharges, color = Churn))+
+  geom_freqpoly(binwidth = 5, size = 1)
+
+#Total charges histogram
+p19 <- ggplot(data = churn_clean, aes(TotalCharges, color = Churn))+
+  geom_freqpoly(binwidth = 200, size = 1)
+
+#Plot quantitative data within a grid
+grid.arrange(p17, p18, p19, ncol=1)
+
+
+
+p20 <- ggplot(churn_clean, aes(x = Churn)) +
+  geom_bar(aes(fill = Churn)) +
+  geom_text(aes(y = ..count.. -200, 
+                label = paste0(round(prop.table(..count..),4) * 100, '%')), 
+            stat = 'count', 
+            position = position_dodge(.1), 
+            size = 3)
+
+churn_clean %>%
+  dplyr::select (TotalCharges, MonthlyCharges, tenure) %>%
+  cor() %>%
+  corrplot.mixed(upper = "circle", tl.col = "black", number.cex = 0.7)
+
+set.seed(56)
+split_train_test <- createDataPartition(churn_clean$Churn,p=0.65,list=FALSE)
+dtrain<- churn_clean[split_train_test,]
+dtest<-  churn_clean[-split_train_test,]
+
+# Remove Total Charges from the training dataset
+dim(dtrain); dim(dtest)
+#dtrain <- dtrain[,-19]
+#dtest <- dtest[,-19]
+
+# Decision Tree analysis
+tr_fit <- rpart(Churn ~., data = dtrain, method="class")
+rpart.plot(tr_fit)
+
+tr_prob1 <- predict(tr_fit, dtest)
+tr_pred1 <- ifelse(tr_prob1[,2] > 0.5,"Yes","No")
+table(Predicted = tr_pred1, Actual = dtest$Churn)
+
+tr_prob2 <- predict(tr_fit, dtrain)
+tr_pred2 <- ifelse(tr_prob2[,2] > 0.5,"Yes","No")
+tr_tab1 <- table(Predicted = tr_pred2, Actual = dtrain$Churn)
+tr_tab2 <- table(Predicted = tr_pred1, Actual = dtest$Churn)
+
+# Train
+confusionMatrix(
+  as.factor(tr_pred2),
+  as.factor(dtrain$Churn),
+  positive = "Yes" 
+)
+
+# Test
+confusionMatrix(
+  as.factor(tr_pred1),
+  as.factor(dtest$Churn),
+  positive = "Yes" 
+)
+
+tr_acc <- sum(diag(tr_tab2))/sum(tr_tab2)
+tr_acc
+
+class(dtrain$Churn)
+dtrain$Churn <- factor(dtrain$Churn, levels = c("No", "Yes"))
+rfModel <- randomForest(Churn ~., data = dtrain)
+print(rfModel)
+
+dtest$Churn <- factor(dtest$Churn)
+
+
+pred_rf <- predict(rfModel, dtest)
+pred_rf <- factor(pred_rf, levels = levels(dtest$Churn))
+levels(pred_rf)
+levels(dtest$Churn)
+caret::confusionMatrix(pred_rf, dtest$Churn)
+plot(rfModel)
+rf_fit2 <- randomForest(Churn ~., data = dtrain, 
+                        ntree = 75, mtry = 2, 
+                        importance = TRUE, proximity = TRUE)
+rf_pred1 <- predict(rf_fit2, dtest)
+table(Predicted = rf_pred1, Actual = dtest$Churn)
+
+
+
+#Display variable importance from random tree
+varImpPlot(rf_fit2, sort=T, n.var = 10, 
+           main = 'Top 10 important variables')
+
+rf_pred2 <- predict(rf_fit2, dtrain)
+rf_tab1 <- table(Predicted = rf_pred2, Actual = dtrain$Churn)
+rf_tab2 <- table(Predicted = rf_pred1, Actual = dtest$Churn)
+
+# Train
+confusionMatrix(
+  as.factor(rf_pred2),
+  as.factor(dtrain$Churn),
+  positive = "Yes" 
+)
+
+# Test
+confusionMatrix(
+  as.factor(rf_pred1),
+  as.factor(dtest$Churn),
+  positive = "Yes" 
+)
+
+rf_acc <- sum(diag(rf_tab2))/sum(rf_tab2)
+rf_acc
+
+
+
+
+
+
+
+
+
